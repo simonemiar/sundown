f6e5861a623c8a4bd816709992ebe5819bd73673
simonemiar <79079392+simonemiar@users.noreply.github.com>
2022-09-08 11:51:06 +0200

    map and marker works

--- pages/test2.vue
+++ pages/test2.vue
@@ -6,17 +6,28 @@
     <div class="m-2 h-80">
       <div>
         <p class="font-bold">Lat:</p>
-
-        <p class="border border-gray-900 w-full h-full">
-          {{ coordinates.missionlongitude }}
-        </p>
+        <!-- <p
+          class="border border-gray-900 w-full h-full"
+          v-model="missionlongitude"
+        ></p> -->
+        <div
+          class="border border-gray-900 w-full h-full"
+          :missionlongitude="missionlongitude"
+        >
+          {{ missionlongitude }}
+        </div>
       </div>
       <div class="h-full">
         <p class="font-bold">Long:</p>
-        <p class="border border-gray-900 w-full">
-          {{ coordinates.missionlatitude }}
-        </p>
+        <!-- <p class="border border-gray-900 w-full" v-model="missionlongitude"> -->
+        <div
+          class="border border-gray-900 w-full"
+          :missionlatitude="missionlongitude"
+        >
+          {{ missionlatitude }}
+        </div>
       </div>
+      <button @click="updateFetch">test latitude</button>
     </div>
   </div>
 </template>
@@ -32,10 +43,8 @@ export default {
       test: 'Test',
       map: '',
       marker: '',
-      coordinates: {
-        missionlongitude: '',
-        missionlatitude: '',
-      },
+      missionlongitude: '',
+      missionlatitude: '',
     }
   },
   head: {
@@ -46,25 +55,28 @@ export default {
       },
     ],
   },
-  created() {
-    let currentTimestamp = Math.floor(Date.now() / 1000)
-    console.log(currentTimestamp)
-    //https://www.youtube.com/watch?v=jKQUHGpOHqg&ab_channel=TheCodingTrain
-    fetch(
-      `https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps=${currentTimestamp}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        this.coordinates.missionlongitude = data.longitude
-        this.coordinates.missionlatitude = data.latitude
-        console.log(this.coordinates.missionlatitude)
-      })
-      .catch((e) => console.log(e))
-  },
   mounted() {
-    this.createMap()
+    this.fetchData()
+    // setInterval(this.updateFetch, 5000)
+    // first we call the function updateFetch which call and sets the data and ends with creating the map
+    // then we call the interval, without the () from the function because if we use them, then it will run the function twice
   },
   methods: {
+    fetchData() {
+      let currentTimestamp = Math.floor(Date.now() / 1000)
+      console.log(currentTimestamp)
+      //https://www.youtube.com/watch?v=jKQUHGpOHqg&ab_channel=TheCodingTrain
+      fetch(
+        `https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps=${currentTimestamp}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          this.missionlongitude = data[0].longitude
+          this.missionlatitude = data[0].latitude
+          this.createMap()
+        })
+        .catch((e) => console.log(e))
+    },
     createMap() {
       const mapboxgl = require('mapbox-gl')
       const map = new mapboxgl.Map({
@@ -72,19 +84,16 @@ export default {
           'pk.eyJ1Ijoic2ltb25lbWlhciIsImEiOiJjbDdybG5ndWowNWFoM3dxcmQwM2Fkd2p6In0.Sz0xWuvcc431FpZdXOwBsQ',
         container: 'map', // <div id="map"></div>
         style: 'mapbox://styles/mapbox/streets-v9', // default style
-        center: [
-          this.coordinates.missionlongitude,
-          this.coordinates.missionlatitude,
-        ], // starting position as [lng, lat]
+        center: [this.missionlongitude, this.missionlatitude], // starting position as [lng, lat]
         zoom: 3,
       })
       // Create a default Marker and add it to the map.
-      const marker1 = new mapboxgl.Marker()
-        .setLngLat([
-          this.coordinates.missionlatitude,
-          this.coordinates.missionlatitude,
-        ])
+      const marker = new mapboxgl.Marker()
+        .setLngLat([this.missionlongitude, this.missionlatitude])
         .addTo(map)
+    },
+    updateFetch() {
+      console.log('update fetch here')
     },
   },
   computed: {
