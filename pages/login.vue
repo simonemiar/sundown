6010d6d0b0a1af3b947bccd058840e3de1fe0c94
simonemiar <79079392+simonemiar@users.noreply.github.com>
2022-09-23 18:33:12 +0200

    fixing login with username or email

--- pages/login.vue
+++ pages/login.vue
@@ -11,17 +11,20 @@
       </div>
       <form @submit.prevent="submitLogin" class="grid m-1 w-full sm:w-1/3">
         <div class="grid">
-          <label for="text">Email</label>
+          <label for="text">Username or Email</label>
           <input
             class="my-1 border-solid border p-1 border-black"
-            id="email"
-            name="email"
-            type="email"
-            v-model="email"
-            placeholder="Please enter your email"
+            id="usernameoremail"
+            name="usernameoremail"
+            type="text"
+            v-model="usernameoremail"
+            placeholder="Please enter your email or username"
+            @keyup="handleUsernameOrEmailChange()"
           />
         </div>
-        <span class="text-red-700" v-if="msg.email">{{ msg.email }}</span>
+        <span class="text-red-700" v-if="msg.usernameoremail">{{
+          msg.usernameoremail
+        }}</span>
         <div class="grid">
           <label for="password">Password:</label>
           <input
@@ -31,9 +34,10 @@
             type="password"
             v-model="password"
             placeholder="Please enter your password"
+            @keyup="handlePasswordChange()"
           />
         </div>
-        <span v-if="msg.password">{{ msg.password }}</span>
+        <span class="text-red-700" v-if="msg.password">{{ msg.password }}</span>
         <button type="submit" class="primary-button my-4">login</button>
       </form>
     </div>
@@ -48,17 +52,69 @@ export default {
   transition: "login",
   data() {
     return {
-      email: "",
+      usernameoremail: "",
       password: "",
-      user: ["johnny"],
+      user: [""],
       msg: [],
     };
   },
   methods: {
+    handleUsernameOrEmailChange() {
+      let checkEmail =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          this.usernameoremail === this.user.email
+        );
+      let checkUsername = users.some((object) => {
+        return object.username === this.usernameoremail;
+      });
+
+      if (checkEmail) {
+        console.log("this is an email");
+        this.msg["usernameoremail"] = "";
+      } else if (checkUsername) {
+        console.log("this is an username");
+        this.msg["usernameoremail"] = "";
+      } else {
+        console.log("this is not an email");
+        this.msg["usernameoremail"] = "Invalid Username or Email";
+      }
+    },
+    handlePasswordChange() {
+      let checkPassword = users.some((object) => {
+        return object.password === this.password;
+      });
+
+      if (checkPassword) {
+        console.log("this is an correct password");
+        this.msg["password"] = "";
+      } else {
+        console.log("this is not an correct password");
+        this.msg["password"] = "Invalid password";
+      }
+    },
     submitLogin() {
+      // let checkUsernameAndEmail = user.email || user.username;
+      // let checkUsernameAndEmail = users.some((object) => {
+      //   return object.email || object.username === this.usernameoremail;
+      // });
+      // let checkEmail = users.find(
+      //   (element) => element.email == this.usernameoremail
+      // );
+      //if ((firstday == 5 || firstday == 6) && totalDays == 31){
+      //Do something
+      //}
+      // let checkUsername = users.find(
+      //   (element) => element.username == this.usernameoremail
+      // );
+
+      // let checkUsernameAndEmail = users.find(
+      //   (element) => element.email || element.username == this.usernameoremail
+      // );
+
       const currentUser = users.find((user) => {
         if (
-          user.email === this.email &&
+          (user.email === this.usernameoremail ||
+            user.username === this.usernameoremail) &&
           user.password === this.password.toString()
         ) {
           return user;
@@ -74,31 +130,31 @@ export default {
         const outputuser = localStorage.getItem("user");
         console.log(outputuser);
       } else {
-        alert("We could not find the user");
+        alert("We could not find the user\nPlease checkout below errors");
       }
     },
-    validateEmail(value) {
-      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
-        this.msg["email"] = "";
-      } else {
-        this.msg["email"] = "Invalid Email Address";
-      }
-    },
-    validatePassword(value) {
-      let difference = 8 - value.length;
-      if (value.length < 8) {
-        this.msg["password"] =
-          "Must be 8 characters! " + difference + " characters left";
-      } else {
-        this.msg["password"] = "";
-      }
-    },
+    // validateEmail(value) {
+    //   if (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value)) {
+    //     this.msg["email"] = "";
+    //   } else {
+    //     this.msg["email"] = "Invalid Email Address";
+    //   }
+    // },
+    // validatePassword(value) {
+    //   let difference = 8 - value.length;
+    //   if (value.length < 8) {
+    //     this.msg["password"] =
+    //       "Must be 8 characters! " + difference + " characters left";
+    //   } else {
+    //     this.msg["password"] = "";
+    //   }
+    // },
   },
-  watch: {
-    email(value) {
-      this.email = value;
-      this.validateEmail(value);
-    },
-  },
+  // watch: {
+  //   email(value) {
+  //     this.email = value;
+  //     this.validateEmail(value);
+  //   },
+  // },
 };
 </script>
